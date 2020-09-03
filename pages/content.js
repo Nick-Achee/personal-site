4a249868ff9d08fd6299a2fda6e1eef86664d61f
Nicholas Achee <nicholasachee@Nicholass-MacBook-Pro-2.local>
2020-09-02 21:36:29 -0700

    delete unused  compontents

--- pages/content.js
+++ pages/content.js
@@ -1,33 +1,44 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Stories from "../components/Stories";
+import Feeds from "../components/Feeds";
+import { Paper, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
-import Stories from '../components/Stories'
-import Feeds from '../components/Feeds'
-import { Grid, Paper, Typography, Divider } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles'
+const useStyles = makeStyles(() => ({
+  pap: {
+    padding: 5,
+    margin: 20,
+  },
 
-const useStyles =  makeStyles(() => ({
-
- pap: {
-  padding: 5,
-  margin: 20,
- },
-
- story_info: {
-   marginTop: '2%',
-  }
+  story_info: {
+    marginTop: "2%",
+  },
 }));
 
 export default function About() {
   const classes = useStyles();
   return (
-    <div><Container><Typography variant="h5" align='center'>BRANDED <br /> ADVERTISEMENT</Typography></Container><Container>
-      <Paper elevation={3} className={classes.pap}><Typography align='center' className={classes.story_info}>Instagram or Facebook Stories/Reels <br /> TikTok | Snapchat <br /> (16:9 Vertical Format) </Typography>
-        <Stories /></Paper>
-        <Paper elevation={3} className={classes.pap}><Typography align='center' className={classes.story_info}>Instagram or Facebook Feeds <br /> (4:5 Vertical Format)   </Typography>
-        <Feeds /></Paper></Container>
-        </div>
+    <div>
+      <Container>
+        <Typography variant="h5" align="center">
+          BRANDED <br /> ADVERTISEMENT
+        </Typography>
+      </Container>
+      <Container>
+        <Paper elevation={3} className={classes.pap}>
+          <Typography align="center" className={classes.story_info}>
+            Instagram or Facebook Stories/Reels <br /> TikTok | Snapchat <br /> (16:9 Vertical Format){" "}
+          </Typography>
+          <Stories />
+        </Paper>
+        <Paper elevation={3} className={classes.pap}>
+          <Typography align="center" className={classes.story_info}>
+            Instagram or Facebook Feeds <br /> (4:5 Vertical Format){" "}
+          </Typography>
+          <Feeds />
+        </Paper>
+      </Container>
+    </div>
   );
 }
